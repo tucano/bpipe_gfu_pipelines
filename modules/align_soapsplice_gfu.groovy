25c30b9fd8042976de2269a5ecbdb3e5ec8cafc0
drambaldi <davide.rambaldi@gmail.com>
2013-10-02 10:17:55 +0200

    Single file pipeline

--- modules/align_soapsplice_gfu.groovy
+++ modules/align_soapsplice_gfu.groovy
@@ -6,74 +6,77 @@ SSPLICEOPT_ALN="-f 2 -q 1 -j 0"
 GFU_PREPARE_ALIGN_SCRIPT = "/home/drambaldi/bpipe_gfu_pipelines/bin/soapsplice_prepare_align.sh"
 GFU_VERIFY_BAM           = "/home/drambaldi/bpipe_gfu_pipelines/bin/verify_bam.sh"
 
+// THIS STAGE SHOULD HANDLE: PAIRED ENDS AND SINGLE ENDS, GZ and FASTQ files
 @Transform("bam")
 align_soapsplice_gfu = 
 {
+    // var to define if the sample is paired or not
+    var paired : true
+    
     doc title: "Soapsplice alignment task",
         desc: "Align with soapsplice. Generate temporary files in /dev/shm on the node",
         author: "davide.rambaldi@gmail.com"
-    // check if input is compressed, this mess is due to the double extension fastq.gz
+
+    EXEC_PREFIX="""
+        HEADER_FILE=`$GFU_PREPARE_ALIGN_SCRIPT $ENVIRONMENT_FILE $input1.gz`;
+        TMP_SCRATCH=`dirname $HEADER_FILE`
+        TMP_OUTPUT_PREFIX=$TMP_SCRATCH/$output.prefix;
+        source $ENVIRONMENT_FILE;        
+        echo -e "[align_soapsplice_gfu]: soapsplice alignment on node $HOSTNAME" >&2;
+    """
+    EXEC_SUFFIX="""
+        cat $HEADER_FILE ${TMP_OUTPUT_PREFIX}.sam | $SAMTOOLS view -Su - | $SAMTOOLS sort - $TMP_OUTPUT_PREFIX
+        $GFU_VERIFY_BAM ${TMP_OUTPUT_PREFIX}.bam || exit 1;
+        mv ${TMP_OUTPUT_PREFIX}.bam ${LOCAL_SCRATCH}/;
+        for F in ${TMP_SCRATCH}/*.junc; do
+            if [[ -e $F ]]; then
+                mv $F ${LOCAL_SCRATCH}/; 
+            fi;
+        done;
+        ln -s ${LOCAL_SCRATCH}/$output.bam $output.bam;
+        rm -rf ${TMP_SCRATCH};
+    """
+    // check if input is compressed
     if (input.endsWith(".gz"))
     {
-        from("*.fastq.gz","*.fastq.gz") produce(input.prefix - ".fastq" + ".bam") 
+        // PAIRED and COMPRESSED      
+        if (paired)
         {
-            exec"""
-                HEADER_FILE=`$GFU_PREPARE_ALIGN_SCRIPT $ENVIRONMENT_FILE $input1.gz`;
-                TMP_SCRATCH=`dirname $HEADER_FILE`
-                TMP_OUTPUT_PREFIX=$TMP_SCRATCH/$output.prefix;
-
-                source $ENVIRONMENT_FILE;
-                
-                echo -e "[align_soapsplice_gfu]: soapsplice alignment on node $HOSTNAME" >&2;
-
-                $SSPLICE -d $REFERENCE_GENOME -1 $input1.gz -2 $input2.gz -o $TMP_OUTPUT_PREFIX -p 4 $SSPLICEOPT_ALN;
-
-                cat $HEADER_FILE ${TMP_OUTPUT_PREFIX}.sam | $SAMTOOLS view -Su - | $SAMTOOLS sort - $TMP_OUTPUT_PREFIX
-
-                $GFU_VERIFY_BAM ${TMP_OUTPUT_PREFIX}.bam || exit 1;
-
-                mv ${TMP_OUTPUT_PREFIX}.bam ${LOCAL_SCRATCH}/;
-
-                for F in ${TMP_SCRATCH}/*.junc; do
-                    if [[ -e $F ]]; then
-                        mv $F ${LOCAL_SCRATCH}/; 
-                    fi;
-                done;
-
-                ln -s ${LOCAL_SCRATCH}/$output.bam $output.bam;
-
-                rm -rf ${TMP_SCRATCH};
-            """, "soapsplice"
+            SSCOMMAND = "$SSPLICE -d $REFERENCE_GENOME -1 $input1.gz -2 $input2.gz -o $TMP_OUTPUT_PREFIX -p 4 $SSPLICEOPT_ALN;"
+            from("*.fastq.gz","*.fastq.gz") produce(input.prefix - ".fastq" + ".bam") 
+            {
+                exec "$EXEC_PREFIX $SSCOMMAND $EXEC_SUFFIX", "soapsplice"
+            }
+        }
+        // SINGLE AND COMPRESSED
+        else
+        {
+            SSCOMMAND = "$SSPLICE -d $REFERENCE_GENOME -1 $input.gz  -o $TMP_OUTPUT_PREFIX -p 4 $SSPLICEOPT_ALN;"
+            from("*.fastq.gz") produce(input.prefix - ".fastq" + ".bam") 
+            {
+                exec "$EXEC_PREFIX $SSCOMMAND $EXEC_SUFFIX", "soapsplice"
+            }
         }
     }
+    // INPUT IS NOT COMPRESSED
     else
     {
-        exec"""
-            HEADER_FILE=`$GFU_PREPARE_ALIGN_SCRIPT $ENVIRONMENT_FILE $input1.gz`;
-            TMP_SCRATCH=`dirname $HEADER_FILE`
-            TMP_OUTPUT_PREFIX=$TMP_SCRATCH/$output.prefix;
-
-            source $ENVIRONMENT_FILE;
-            
-            echo -e "[align_soapsplice_gfu]: soapsplice alignment on node $HOSTNAME" >&2;
-
-            $SSPLICE -d $REFERENCE_GENOME -1 $input1 -2 $input2 -o $TMP_OUTPUT_PREFIX -p 4 $SSPLICEOPT_ALN;
-
-            cat $HEADER_FILE ${TMP_OUTPUT_PREFIX}.sam | $SAMTOOLS view -Su - | $SAMTOOLS sort - $TMP_OUTPUT_PREFIX
-
-            $GFU_VERIFY_BAM ${TMP_OUTPUT_PREFIX}.bam || exit 1;
-
-            mv ${TMP_OUTPUT_PREFIX}.bam ${LOCAL_SCRATCH}/;
-
-            for F in ${TMP_SCRATCH}/*.junc; do
-                if [[ -e $F ]]; then
-                    mv $F ${LOCAL_SCRATCH}/; 
-                fi;
-            done;
-
-            ln -s ${LOCAL_SCRATCH}/$output.bam $output.bam;
-
-            rm -rf ${TMP_SCRATCH};
-        """, "soapsplice"  
+        // PAIRED
+        if (paired)
+        {
+            from("*.fastq","*.fastq") produce(input.prefix - ".fastq" + ".bam") 
+            {
+                SSCOMMAND = "$SSPLICE -d $REFERENCE_GENOME -1 $input1 -2 $input2 -o $TMP_OUTPUT_PREFIX -p 4 $SSPLICEOPT_ALN;"
+            }
+        }
+        // SINGLE
+        else
+        {
+            from("*.fastq") produce(input.prefix - ".fastq" + ".bam") 
+            {
+                SSCOMMAND = "$SSPLICE -d $REFERENCE_GENOME -1 $input  -o $TMP_OUTPUT_PREFIX -p 4 $SSPLICEOPT_ALN;"
+            }
+        }
+        exec "$EXEC_PREFIX $SSCOMMAND $EXEC_SUFFIX", "soapsplice" 
     }
 }
--- test/rna_soapsplice_single_pair/runner.sh
+++ test/rna_soapsplice_single_pair/runner.sh
@@ -4,7 +4,7 @@
 OUTPUT="read1_0000.merge.bam"
 
 # SCRIPT_NAME
-SCRIPT_NAME="rna_soapsplice_single_files"
+SCRIPT_NAME="rna_soapsplice_single_pair"
 
 # 1. run cleaner
 ./cleaner.sh
@@ -13,10 +13,10 @@ SCRIPT_NAME="rna_soapsplice_single_files"
 ../../bin/pbs_config.sh
 
 # 3. call prepare scripts (example)
-../../bin/soapsplice_prepare_single_file.sh -n "test_rna_soapsplice_single_file" -g hg19 /lustre1/genomes /lustre2/scratch /lustre1/tools/bin/soapsplice > gfu_environment.sh
+../../bin/soapsplice_prepare_single_file.sh -n "test_rna_soapsplice_single_pair" -g hg19 /lustre1/genomes /lustre2/scratch /lustre1/tools/bin/soapsplice > gfu_environment.sh
 
 # 4. run bipe
-bpipe run ../../pipelines/rna_soapsplice/soapsplice_submit_singlefile.groovy *R*.gz
+bpipe run ../../pipelines/rna_soapsplice/soapsplice_submit_singlepair.groovy *R*.gz
 
 # 5. convert output to sam and return 0 or >0
 /usr/local/cluster/bin/samtools view -h $OUTPUT | diff - expected.sam > /dev/null 2>&1
