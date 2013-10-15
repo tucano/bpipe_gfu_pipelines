dc5e573b6f47490a427ea49da0d486d769e0674c
drambaldi <davide.rambaldi@gmail.com>
2013-10-15 19:42:04 +0200

    Refactoring pipes

---
+++ modules/set_stripe_gfu.groovy
@@ -0,0 +1,19 @@
+// MODULE CONFIG LUSTRE FS
+LSF="/usr/bin/lfs"
+
+set_stripe_gfu =
+{
+    doc title: "Set lustre options for working directory",
+        desc: """
+            Lustre options: 
+                -c -1 : a stripe_count of -1 means to stripe over all available OSTs.
+                -i -1 : a start_ost_index of -1 allows the MDS to choose the starting index and it is strongly recommended, as this allows space and load balancing to be done by the MDS as needed.
+                -s 2M : Stripsize 2 megabytes
+        """,
+        constraints: "It is a non blocking stage (Fails in non lustre fs, but will return always true).",
+        author: "davide.rambaldi@gmail.com"
+
+    exec """
+        $LSF setstripe -c -1 -i -1 -s 2M . 1>/dev/null 2>&1 || true;
+    """
+}
--- test/dna_bwa_lane/bpipeconfig_1_test_bwa_submit_lane.groovy
+++ test/dna_bwa_lane/bpipeconfig_1_test_bwa_submit_lane.groovy
@@ -3,15 +3,20 @@ about title: "DNA alignment with bwa (lane): IOS GFU 009"
 // Usage line will be used to infer the correct bpipe command
 // USAGE: bpipe run -r $pipeline_filename *.fastq.gz
 
-// PROJECT VARS will be added by bpipe-config
+// PROJECT VARS Should be changed by bpipe-config
 // I don't wanna templates for a groovy file. Use simple regexp with PLACEHOLDERS
 // Don't change my keywords in source!
 
-REFERENCE_GENOME = "/lustre1/genomes/BPIPE_REFERENCE_GENOME/bwa/BPIPE_REFERENCE_GENOME"
+REFERENCE_GENOME = "/lustre1/genomes/hg19/bwa/hg19"
 PLATFORM         = "illumina"
 CENTER           = "CTGB"
 
-//--BPIPE_ENVIRONMENT_HERE--
+PROJECTNAME="bpipeconfig_1_test"
+REFERENCE="hg19"
+EXPERIMENT_NAME="D2A8DACXX_B1"
+FCID="D2A8DACXX"
+LANE="3"
+SAMPLEID="B1"
 
 
 /*
@@ -20,6 +25,6 @@ CENTER           = "CTGB"
  * Use -I for base64 Illumina quality
  */ 
 Bpipe.run {
-    set_stripe_gfu + "%.fastq.gz" * [align_bwa_gfu.using(BWAOPT_ALN: "-q 30")] +
-    "_R*_%.sai" * [sam_bwa_gfu.using(BWAOPT_PE: "", paired: true, lane: true, compressed : true)]
+    set_stripe_gfu + "%.fastq.gz" * [align_bwa_gfu.using(BWAOPT_ALN: "-q 30")] + 
+	"_R*_%.sai" * [sam_bwa_gfu.using(BWAOPT_PE: "", paired: true, lane: true, compressed : true)]
 }
