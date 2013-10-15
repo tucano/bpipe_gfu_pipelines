02a0c56e38a374d0aa72dc8efb73bba9db1ba8b9
drambaldi <davide.rambaldi@gmail.com>
2013-10-15 14:32:41 +0200

    added the pipeline

---
+++ pipelines/dna_bwa/bwa_submit_lane.groovy
@@ -0,0 +1,15 @@
+about title: "DNA alignment with bwa (lane): IOS GFU 009"
+
+// GENERAL ENVIROMENT VARS
+ENVIRONMENT_FILE = "gfu_environment.sh"
+SCRATCH_PREFIX   = "/lustre2/scratch"
+
+// PROJECT NAME Should be changed by bpipe-config
+PROJECT_NAME     = ""
+
+// Usage line will be used to infer the correct bpipe command
+// USAGE: bpipe run -r $pipeline_filename *.fastq.gz
+
+Bpipe.run {
+
+}
